159c7a1adc687d21aa0798781d815ba6ab4cd319
Shu Song <shoebox@gmail.com>
2017-01-14 16:37:19 -0500

    mangling the code now

--- webpack.config.js
+++ webpack.config.js
@@ -7,19 +7,7 @@ module.exports = {
     path: "dist",
     filename: "out.js"
   },
-  // module: {
-  //   loaders: [
-  //     {
-  //       test: /\.js$/,
-  //       exclude: /(node_modules|bower_components)/,
-  //       loader: 'babel-loader',
-  //       query: {
-  //         presets: ['es2015']
-  //       }
-  //     }
-  //   ],
-  // },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    new webpack.optimize.UglifyJsPlugin(),
   ]
 };
